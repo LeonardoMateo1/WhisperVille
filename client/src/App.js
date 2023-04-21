d391c82e5c586d5f64a2a75bfc7fa427b14acbb1
LeonardoMateo1 <104122352+LeonardoMateo1@users.noreply.github.com>
2023-04-21 18:50:24 -0400

    basic setup for front-end w dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Landing from './components/Landing';
 
 function App() {
   return (
-
+    <BrowserRouter>
       <div className="App">
-        
+        <Routes>
+          <Route path='/' element={<Landing/>}/>
+        </Routes>
       </div>
-
+    </BrowserRouter>
   );
 }
 
